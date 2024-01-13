3c0c447a356e4bef2b33b367e274f10e6abe31dc
Apoorv Gadiya <apoorvgadiya@gmail.com>
2024-01-13 09:07:26 +0530

    DAY 2 (REFRESHED)

---
+++ day-2/index.js
@@ -0,0 +1,32 @@
+// let x,y,z;
+// x=Math.floor(((Math.random()*6)+1));// Math.Random
+// console.log(x);
+
+
+
+// const PI=null;
+// PI=3.1415;       // const
+// console.log(PI);
+
+
+
+// let count=0;
+// document.getElementsByClassName("up")[0].onclick=()=>
+// {
+//     count++;
+//     document.querySelector(".container p").textContent=count;
+// };
+// document.getElementsByClassName("down")[0].onclick=()=>
+// {
+//     count--;                                 
+//     document.querySelector(".container p").textContent=count;       //counter
+// };
+   
+// document.getElementsByClassName("reset")[0].onclick=()=>
+// {
+//     count=0;
+//     document.querySelector(".container p").textContent=count;
+// };
+   
+   
+
