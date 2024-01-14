9927035e3d4ac66939a521803dfe8d4b218fc986
Apoorv Gadiya <apoorvgadiya@gmail.com>
2024-01-14 22:25:55 +0530

    Day-4

---
+++ day-4/index.js
@@ -0,0 +1,14 @@
+// let myNum=123456.789;
+
+// Number system
+// myNum=myNum.toLocaleString("en-US");//US -English
+// myNum=myNum.toLocaleString("hi-IN");//IND -Hindi 
+
+// Currency
+// myNum=myNum.toLocaleString("en-US",{style:"currency",currency:"USD"});
+// myNum=myNum.toLocaleString("en-IN",{style:"currency",currency:"INR"});
+
+// Other stuff
+// myNum=myNum.toLocaleString(undefined,{style:"percent"})
+// myNum=myNum.toLocaleString(undefined,{style:"unit",unit:"celsius"})
+// console.log(myNum)
